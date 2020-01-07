2b9048a68002ea18519ba6c9de892166b8e53014
skywarth <yigitk.ersoy@gmail.com>
2020-01-07 16:46:04 +0300

    implementation of SMO (almost)

--- vanillaSVM.js
+++ vanillaSVM.js
@@ -19,8 +19,73 @@ class SVM {
             this._alpha.push(0);
         }
 
-
+        this.sequentialMinimalOptimization();
     }
+
+    sequentialMinimalOptimization() {
+        let passes = 0;
+        while (passes < this._maxPasses) {
+            let didAlphaChanged = 0;
+            for (let i = 0; i < this._N; i++) {
+                const Ei = this.dualClassification(this._data[i]) - this._labels[i];
+                // find data that violates KKT conditions
+                if ((this._labels[i] * Ei < -this._tol && this._alpha[i] < this._C) || (this._labels[i] * Ei > this._tol && this._alpha[i] > 0)) {
+                    let j = i;
+                    while (j === i) {
+                        j = random.randomInt(0, this._N);
+                    }
+                    const Ej = this.dualClassification(this._data[j]) - this._labels[j];
+                    let ai = this._alpha[i],
+                        aj = this._alpha[j];
+                    let L = 0,
+                        H = this._C;
+                    if (this._labels[i] === this._labels[j]) {
+                        L = Math.max(0, ai + aj - this._C);
+                        H = Math.min(this._C, ai + aj);
+                    } else {
+                        L = Math.max(0, aj - ai);
+                        H = Math.min(this._C, this._C + aj - ai);
+                    }
+                    if (Math.abs(L - H) < 1e-4) {
+                        continue;
+                    }
+                    const eta = 2 * this.kernel(this._data[i], this._data[j]) - this.kernel(this._data[i], this._data[i]) - this.kernel(this._data[j], this._data[j]);
+                    if (eta >= 0) {
+                        continue;
+                    }
+                    let newaj = aj - (this._labels[j] * (Ei - Ej)) / eta;
+                    if (newaj > H) {
+                        newaj = H;
+                    } else if (newaj < L) {
+                        newaj = L;
+                    }
+                    if (Math.abs(aj - newaj) < 0.00001) {
+                        continue;
+                    }
+                    this._alpha[j] = newaj;
+                    let newai = ai + this._labels[i] * this._labels[j] * (aj - newaj);
+                    this._alpha[i] = newai;
+
+                    const b1 = this._b - Ei - this._labels[i] * (newai - ai) * this.kernel(this._data[i], this._data[i]) - this._labels[j] * (newaj - aj) * this.kernel(this._data[i], this._data[j]);
+                    const b2 = this._b - Ej - this._labels[i] * (newai - ai) * this.kernel(this._data[i], this._data[j]) - this._labels[j] * (newaj - aj) * this.kernel(this._data[j], this._data[j]);
+
+                    if (newai > 0 && newai < this._C) {
+                        this._b = b1;
+                    } else if (newaj > 0 && newaj < this._C) {
+                        this._b = b2;
+                    } else {
+                        this._b = 0.5 * (b1 + b2);
+                    }
+                    didAlphaChanged += 1;
+                } //end of for-loop related to j
+                if (didAlphaChanged === 0) {
+                    passes += 1;
+                } else {
+                    passes = 0;
+                }
+            } //end of for-loop related to i
+        } //end while
+    } //end of function
 
 
 
