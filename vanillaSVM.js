0eaa05530c969b91133d016a7740959b62961d04
skywarth <yigitk.ersoy@gmail.com>
2020-01-07 15:26:24 +0300

    kernel func for gaussian

--- vanillaSVM.js
+++ vanillaSVM.js
@@ -4,4 +4,18 @@ class SVM {
     constructor(data, labels, C, kernel, RBFSigma = 15) {
 
     }
+
+
+
+    //prepare kernel
+    //ps only for gaussian...
+    kernel(Xi, Xj) {
+        switch (this._kernel) {
+            case 'RBF':
+                return kernels.gaussianKernel(Xi, Xj, this._RBFSigma);
+            default:
+                return 'ILLEGAL KERNEL';
+        }
+    }
+
 }
